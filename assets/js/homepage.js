bb2628d1966d4d8085d5aa80842e85a551fec188
Freelancer Jabir <devzabir200@gmail.com>
2022-01-17 23:39:42 +0600

    ver-3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,51 +5,6 @@
 //   Description : Digital Business Agency Bootstrap and Html Template
 //   Version : 1.0 
 
-
-// Owl Carousel Start
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items: 4,
-    loop: true,
-    vav: false,
-    margin: 20,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    responsive: {
-        0: {
-            items: 2
-        },
-        400: {
-            items: 3
-        },
-        600: {
-            items: 3
-        },
-        960: {
-            items: 5
-        },
-        1200: {
-            items: 6
-        }
-    },
-    autoplayHoverPause: true
-});
-$('.play').on('click', function() {
-    owl.trigger('play.owl.autoplay', [1000])
-})
-$('.stop').on('click', function() {
-    owl.trigger('stop.owl.autoplay')
-})
-owl.on('mousewheel', '.owl-stage', function(e) {
-    if (e.deltaY > 0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
-// OWL Carousel End
-
 // Swiper Js Script
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
@@ -72,4 +27,10 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
-// Swiper Js End 
+// Swiper Js End
+
+// Skills Play Button Start
+$("#playbtn").click(function() {
+    
+});
+// Skills Play Button End
